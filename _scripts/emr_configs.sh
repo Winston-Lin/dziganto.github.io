93d58057fc6558704f20ca279b578d5d44f145e3
David Ziganto <dziganto@users.noreply.github.com>
2017-06-04 23:21:04 -0500

    Add files via upload

--- _scripts/emr_configs.sh
+++ _scripts/emr_configs.sh
@@ -13,11 +13,9 @@ sudo ln -s /mnt/usr-moved/share /usr/
 # ----------------------------------------------------------------------
 #              Install Anaconda (Python 3) & Set To Default              
 # ----------------------------------------------------------------------
-wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh -O ~/anaconda.sh
+wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh -O ~/anaconda.sh
 bash ~/anaconda.sh -b -p $HOME/anaconda
 echo -e '\nexport PATH=$HOME/anaconda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
-conda create -n py35 python=3.5 -y
-echo -e '\nsource activate py35' >> $HOME/.bashrc && source $HOME/.bashrc
 
 # ----------------------------------------------------------------------
 #                    Install Additional Packages              
@@ -30,7 +28,7 @@ pip install textblob selenium
 # ----------------------------------------------------------------------
 conda create -yn tensorflow
 source activate tensorflow
-pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.0.1-cp36-cp36m-linux_x86_64.whl
+pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.1.0-cp35-cp35m-linux_x86_64.whl
 source deactivate
 
 # ----------------------------------------------------------------------
